e37e2d13857d166dd7941d5b0f5e5bf10aa38200
Kentypa <12orator12@gmail.com>
2024-12-13 00:02:51 +0200

    Progress in phone regular expression

--- Level-1/JS 1,1/Regular Expressions.js
+++ Level-1/JS 1,1/Regular Expressions.js
@@ -5,16 +5,26 @@ function Validator () {
     }
 
     this.validatePhone = (phone) => {
-        let regexp = /%/
+        let regexp =  /^((\+\d\d|([- ]{*}|))+([ -]|))(\(|[ -])([0-9][ -]?[0-9][ -]?[0-9][ -]?)/
+        return phone.match(regexp);
     }
 }
 
-console.log(new Validator().validateEmail("fi@secondpart.end"))
-console.log(new Validator().validateEmail("first-part@.se=cond%p.art.end"))
-console.log(new Validator().validateEmail("first.part@se=cond%part.r"))
-console.log(new Validator().validateEmail("f@secondart.end,"))
-console.log(new Validator().validateEmail("first-part@.se=cond@part.end"))
-console.log(new Validator().validateEmail("-firstpart@.se=cond%.enddeded"))
-console.log(new Validator().validateEmail("firs_tpart@.se.en"))
-console.log(new Validator().validateEmail(""))
-console.log(new Validator().validateEmail("firstpart@.se.enddeded"))
+// console.log(new Validator().validateEmail("fi@secondpart.end")) // Валідні
+// console.log(new Validator().validateEmail("first-part@.se=cond%p.art.end")) // Валідні
+// console.log(new Validator().validateEmail("first.part@se=cond%part.r")) // Валідні
+// console.log(new Validator().validateEmail("f@secondart.end,")) // Не валідні
+// console.log(new Validator().validateEmail("first-part@.se=cond@part.end")) // Не валідні
+// console.log(new Validator().validateEmail("-firstpart@.se=cond%.enddeded")) // Не валідні
+// console.log(new Validator().validateEmail("firs_tpart@.se.en")) // Не валідні
+// console.log(new Validator().validateEmail("")) // Не валідні
+// console.log(new Validator().validateEmail("firstpart@.se.enddeded")) // Не валідні
+
+console.log(new Validator().validatePhone("+38 (099) 567 8901")) // Валідні
+console.log(new Validator().validatePhone("+38 099 5 6 7 8 9  01")) // Валідні
+console.log(new Validator().validatePhone("(09-9) 567-890-1")) // Валідні
+console.log(new Validator().validatePhone("--  (099) 567 890-1")) // Валідні
+console.log(new Validator().validatePhone("+38 (099) 567 8901 0")) // Не Валідні
+console.log(new Validator().validatePhone("+38 099 a0000000")) // Не Валідні
+console.log(new Validator().validatePhone("+38 (0989) 567 8901")) // Не Валідні
+console.log(new Validator().validatePhone("+48 (0989) 567 8901"))// Не Валідні
